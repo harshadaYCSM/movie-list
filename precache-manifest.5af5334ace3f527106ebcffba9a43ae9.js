68b2f74b97b9fc9182232aebb987e01299712177
Harshada Chaudhari <harshada.chaudhari@ltts.com>
2020-08-30 21:44:05 +0530

    Updates

---
+++ precache-manifest.5af5334ace3f527106ebcffba9a43ae9.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c918faaa096c0e3dcf48bd91b1daf976",
+    "url": "/contentList/index.html"
+  },
+  {
+    "revision": "0abfac62e7f21fabb221",
+    "url": "/contentList/static/css/3.a21bace2.chunk.css"
+  },
+  {
+    "revision": "acee8489ac3627a68d3d",
+    "url": "/contentList/static/css/main.31affb8f.chunk.css"
+  },
+  {
+    "revision": "035e4668dd61b60e3d0a",
+    "url": "/contentList/static/js/2.19bbd421.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/contentList/static/js/2.19bbd421.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0abfac62e7f21fabb221",
+    "url": "/contentList/static/js/3.f200b333.chunk.js"
+  },
+  {
+    "revision": "f3a34993a522629606ef",
+    "url": "/contentList/static/js/4.196ecd5f.chunk.js"
+  },
+  {
+    "revision": "1e4b6dbbf7ea8d8215f1",
+    "url": "/contentList/static/js/5.f6e6d0bf.chunk.js"
+  },
+  {
+    "revision": "933db34f37f54dc39c2d",
+    "url": "/contentList/static/js/6.4fb2fa35.chunk.js"
+  },
+  {
+    "revision": "acee8489ac3627a68d3d",
+    "url": "/contentList/static/js/main.f329245d.chunk.js"
+  },
+  {
+    "revision": "e2bf0886e010065412da",
+    "url": "/contentList/static/js/runtime-main.f3a453e0.js"
+  }
+]);
